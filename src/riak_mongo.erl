29ed8657023957849a67abe1e68e0d6b7eaf382a
Drew Kerrigan <dkerrigan@basho.com>
2014-02-04 15:01:01 -0500

    refactoring

--- src/riak_mongo.erl
+++ src/riak_mongo.erl
@@ -1,36 +1,30 @@
-%%
-%% This file is part of riak_mongo
-%%
-%% Copyright (c) 2012 by Pavlo Baron (pb at pbit dot org)
-%%
-%% Licensed under the Apache License, Version 2.0 (the "License");
-%% you may not use this file except in compliance with the License.
-%% You may obtain a copy of the License at
-%%
-%% http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing, software
-%% distributed under the License is distributed on an "AS IS" BASIS,
-%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-%% See the License for the specific language governing permissions and
-%% limitations under the License.
-%%
 
-%% @author Pavlo Baron <pb at pbit dot org>
-%% @doc This is the main module of riak_mongo
-%% @copyright 2012 Pavlo Baron
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2014 Basho Technologies, Inc.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+%% @doc riak_mongo external api
 
 -module(riak_mongo).
--behaviour(application).
--export([start/2, stop/1]).
+-export([
+    process_bson/1
+    ]).
 
--define(DEFAULT_ADDR, "127.0.0.1").
--define(DEFAULT_PORT, 32323).
-
-start(_Type, _StartArgs) ->
-    IpAddr = app_helper:get_env(riak_mongo, ip, ?DEFAULT_ADDR),
-    Port = app_helper:get_env(riak_mongo, port, ?DEFAULT_PORT),
-    riak_mongo_sup:start_link(IpAddr, Port).
-
-stop(_State) ->
+process_bson(Bson) ->
     ok.
--- test/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir mongo
-cd mongo
-git init
-git config core.sparsecheckout true
-echo jstests/ >> .git/info/sparse-checkout
-git remote add origin https://github.com/mongodb/mongo.git
-git pull origin master
